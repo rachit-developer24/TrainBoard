f4992b2b0b8c3121ff709616697b8f77fc09cfa6
rachit-developer24 <srachit841@gmail.com>
2026-04-12 18:00:04 +0100

    Initial Commit

---
+++ TrainBoard/TrainBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainBoardApp.swift
+//  TrainBoard
+//
+//  Created by Rachit Sharma on 12/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct TrainBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
